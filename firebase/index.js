9f3b9c6c232a1d32577fa0c133e01c3c7a8b54c3
AdityaKumar2408 <adityachouhan7705@gmail.com>
2024-05-18 23:50:08 +0530

    do this

---
+++ firebase/index.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYY6GvUyMzyqZqF0QhJ-w6YHDH1wOgHSI",
+  authDomain: "todo-list-e3257.firebaseapp.com",
+  projectId: "todo-list-e3257",
+  storageBucket: "todo-list-e3257.appspot.com",
+  messagingSenderId: "985887909608",
+  appId: "1:985887909608:web:256e9d2808774fa35b4b5d"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
