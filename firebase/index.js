6e58b38bc455dabe6b98920f4d7cdcd9c359fdd7
Aditya Kumar <110921916+AdityaKumar2408@users.noreply.github.com>
2024-05-19 08:43:28 +0530

    Update index.js

--- firebase/index.js
+++ firebase/index.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID
+  // apiKey: process.env.API_KEY,
+  // authDomain: process.env.AUTH_DOMAIN,
+  // projectId: process.env.PROJECT_ID,
+  // storageBucket: process.env.STORAGE_BUCKET,
+  // messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  // appId: process.env.APP_ID
 apiKey:"AIzaSyBYY6GvUyMzyqZqF0QhJ-w6YHDH1wOgHSI",
 authDomain:"todo-list-e3257.firebaseapp.com",
 projectId:"todo-list-e3257",
